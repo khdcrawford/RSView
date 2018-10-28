6537cc213e7150ac06e51f1a34994569535e9b1f
Kate Dusenbury <khdusenbury@gmail.com>
2018-10-27 22:44:12 -0700

    separated script into functions

--- seq_download.py
+++ seq_download.py
@@ -2,25 +2,44 @@ from Bio import Entrez
 
 Entrez.email = "dusenk@uw.edu"
 
-search_handle = Entrez.esearch(db="nuccore", retmax=100, 
-		term="human respiratory syncytial virus G")
-search_record = Entrez.read(search_handle)
-search_handle.close()
-search_IDs = search_record['IdList']
+database = "nuccore"
+maxseqs = 20
+query = "human respiratory syncytial virus G"
+filetype = "gb"
+outmode = "xml"
 
-print('Search returned {0} hits.'.format(len(search_IDs)))
 
-handle = Entrez.efetch(db="nuccore", id=search_IDs,
-		rettype="gb", retmode="xml")
-parsed = Entrez.parse(handle)
-records = list(parsed)
-handle.close()
+def getIDs(db, retmax, term):
+	search_handle = Entrez.esearch(db=db, retmax=retmax, 
+		term=term)
+	search_record = Entrez.read(search_handle)
+	search_handle.close()
+	search_IDs = search_record['IdList']
+	print('Search returned {0} hits.'.format(len(search_IDs)))
+	return(search_IDs)
 
-for i in range(len(records)):
-	record = records[i]
-	features = record['GBSeq_feature-table']
-	for feature in features:
-		featquals = feature['GBFeature_quals']
-		for qual in featquals:
-			qual_dict = dict(qual)
-			#print(qual_dict['GBQualifier_name'] + ": " + qual_dict['GBQualifier_value'])
+def getrecords(db, ids, start, rettype, retmode):
+	handle = Entrez.efetch(db=db, id=ids, retstart = start, rettype=rettype, 
+			retmode=retmode)
+	parsed = Entrez.parse(handle)
+	records = list(parsed)
+	handle.close()
+	return(records)
+
+def makedf(records):
+	for i in range(len(records)):
+		record = records[i]
+		features = record['GBSeq_feature-table']
+		for feature in features:
+			featquals = feature['GBFeature_quals']
+			for qual in featquals:
+				qual_dict = dict(qual)
+				#print(qual_dict['GBQualifier_name'] + ": " + qual_dict['GBQualifier_value'])
+
+def main():
+	IDs = getIDs(database, maxseqs, query)
+	records = getrecords(database, IDs, 0, filetype, outmode)
+	makedf(records)
+
+if __name__ == '__main__':
+	main()

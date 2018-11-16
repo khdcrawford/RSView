6b95b2563ccecf4eb72d7cbffbb05f0e47d002d3
Kate Dusenbury <khdusenbury@gmail.com>
2018-11-15 19:13:54 -0800

    Continued parsing genbank data
    Outputs higher level strain data for each entry to a pandas dataframe.

--- scripts/seq_download.py
+++ scripts/seq_download.py
@@ -1,16 +1,17 @@
 from Bio import Entrez
+import pandas as pd
 
 Entrez.email = "dusenk@uw.edu"
 
 database = "nuccore"
-maxseqs = 20
+maxseqs = 1000
 query = "human respiratory syncytial virus G"
 filetype = "gb"
 outmode = "xml"
 
 
 def getIDs(db, retmax, term):
-	search_handle = Entrez.esearch(db=db, retmax=retmax, 
+	search_handle = Entrez.esearch(db=db, retmax=retmax,
 		term=term)
 	search_record = Entrez.read(search_handle)
 	search_handle.close()
@@ -19,22 +20,41 @@ def getIDs(db, retmax, term):
 	return(search_IDs)
 
 def getrecords(db, ids, start, rettype, retmode):
-	handle = Entrez.efetch(db=db, id=ids, retstart = start, rettype=rettype, 
-			retmode=retmode)
+	handle = Entrez.efetch(db=db, id=ids, retstart=start, rettype=rettype,
+		retmode=retmode)
 	parsed = Entrez.parse(handle)
 	records = list(parsed)
 	handle.close()
 	return(records)
 
 def makedf(records):
+	date_dict = {}
+	except_count = 0
 	for i in range(len(records)):
+		if i % 100 == 0:
+			print('{0} records processed'.format(i))
+		sub_dict = {}
 		record = records[i]
 		features = record['GBSeq_feature-table']
-		for feature in features:
-			featquals = feature['GBFeature_quals']
-			for qual in featquals:
-				qual_dict = dict(qual)
-				#print(qual_dict['GBQualifier_name'] + ": " + qual_dict['GBQualifier_value'])
+		strain_quals = features[0]['GBFeature_quals']
+		for qual in strain_quals:
+			qual_dict = dict(qual)
+			sub_dict[qual_dict['GBQualifier_name']] = \
+					qual_dict['GBQualifier_value']
+		try:
+			date_dict[sub_dict['collection_date']] = sub_dict
+		except:
+			except_count += 1
+			print(sub_dict)
+			continue
+
+	date_df = pd.DataFrame.from_dict(date_dict, orient='index')
+	print(date_df.drop(['db_xref', 'mol_type', 'collection_date', 'lat_lon', 'note', 'host', 'isolate', 'isolation_source'], axis=1))
+	print(except_count)
+	print(len(date_df))
+
+	#print(strain_df.drop(['db_xref', 'mol_type', 'strain', 'lat_lon', 'note', 'host'], axis=1))
+	#return strain_dict
 
 def main():
 	IDs = getIDs(database, maxseqs, query)

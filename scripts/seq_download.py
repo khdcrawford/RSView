cbfc3a6eb73882aeb31cebb2b47247e137aa776f
Kate Dusenbury <khdusenbury@gmail.com>
2018-11-16 08:30:49 -0800

    downloaded first batch of metadata

--- scripts/seq_download.py
+++ scripts/seq_download.py
@@ -4,7 +4,7 @@ import pandas as pd
 Entrez.email = "dusenk@uw.edu"
 
 database = "nuccore"
-maxseqs = 1000
+maxseqs = 5000
 query = "human respiratory syncytial virus G"
 filetype = "gb"
 outmode = "xml"
@@ -17,49 +17,40 @@ def getIDs(db, retmax, term):
 	search_handle.close()
 	search_IDs = search_record['IdList']
 	print('Search returned {0} hits.'.format(len(search_IDs)))
-	return(search_IDs)
+	return search_IDs
 
-def getrecords(db, ids, start, rettype, retmode):
+def gethandle(db, ids, start, rettype, retmode):
 	handle = Entrez.efetch(db=db, id=ids, retstart=start, rettype=rettype,
 		retmode=retmode)
-	parsed = Entrez.parse(handle)
-	records = list(parsed)
-	handle.close()
-	return(records)
+	return handle
 
-def makedf(records):
-	date_dict = {}
-	except_count = 0
-	for i in range(len(records)):
-		if i % 100 == 0:
-			print('{0} records processed'.format(i))
+
+def makedf(handle):
+	records = Entrez.parse(handle)
+	seqinfo = []
+	for record in records:
 		sub_dict = {}
-		record = records[i]
 		features = record['GBSeq_feature-table']
 		strain_quals = features[0]['GBFeature_quals']
 		for qual in strain_quals:
 			qual_dict = dict(qual)
 			sub_dict[qual_dict['GBQualifier_name']] = \
 					qual_dict['GBQualifier_value']
-		try:
-			date_dict[sub_dict['collection_date']] = sub_dict
-		except:
-			except_count += 1
-			print(sub_dict)
-			continue
+		seqinfo.append(sub_dict)
+	
+	handle.close()
 
-	date_df = pd.DataFrame.from_dict(date_dict, orient='index')
-	print(date_df.drop(['db_xref', 'mol_type', 'collection_date', 'lat_lon', 'note', 'host', 'isolate', 'isolation_source'], axis=1))
-	print(except_count)
-	print(len(date_df))
+	seqinfo_df = pd.DataFrame(seqinfo)
 
-	#print(strain_df.drop(['db_xref', 'mol_type', 'strain', 'lat_lon', 'note', 'host'], axis=1))
-	#return strain_dict
+	return seqinfo_df
+
 
 def main():
 	IDs = getIDs(database, maxseqs, query)
-	records = getrecords(database, IDs, 0, filetype, outmode)
-	makedf(records)
+	handle = gethandle(database, IDs, 0, filetype, outmode)
+	metadata_df = makedf(handle)
+	metadata_df.to_csv('../data/RSVG_gb_metadata.csv')
+	print(metadata_df.drop(['db_xref', 'host', 'mol_type'], axis=1))
 
 if __name__ == '__main__':
 	main()

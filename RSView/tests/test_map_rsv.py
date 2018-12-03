357f7fea50f7fdb6bb23e7d51bd77e02f1a8365b
kistlerk <kistlerk@uw.edu>
2018-12-02 20:21:26 -0800

    add environment.yml

--- RSView/tests/test_map_rsv.py
+++ RSView/tests/test_map_rsv.py
@@ -1,7 +1,22 @@
 import unittest
-
-import map_rsv
+import os
+from . import map_rsv
 
 class TestMapRsv(unittest.TestCase):
-    def test_countries(self):
-        assertTrue(len(organized_df[organized_df['adj_lon'].isnull()])==0)
+    """
+    Tests map_rsv.py
+    """
+
+    def test_df_structure(self):
+        rsv_df = map_rsv.organize_data(map_rsv.DATAFILES, map_rsv.GENOTYPE_DICT)
+
+        self.assertEqual(list(rsv_df.columns), ['collection_date', 'country', 'genotype',
+                         'subtype', 'year', 'genotype_group'])
+
+
+
+    # def test_countries(self):
+    #     assertTrue(len(organized_df[organized_df['adj_lon'].isnull()])==0)
+
+if __name__ == '__main__':
+    unittest.main()

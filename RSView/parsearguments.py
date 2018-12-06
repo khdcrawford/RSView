4c0aebcecb33d327607d635d6c705b3f76de9fe6
Jilliane Bruffey <jillianeb@gmail.com>
2018-12-06 00:48:02 -0800

    updated plot_correlation to plot yearly data

--- RSView/parsearguments.py
+++ RSView/parsearguments.py
@@ -8,111 +8,130 @@ import RSView
 from argparse import RawTextHelpFormatter
 
 class ArgumentParserNoArgHelp(argparse.ArgumentParser):
-    """Like *argparse.ArgumentParser*, but prints help when no arguments."""
-    def error(self, message):
-        """Prints error message, then help."""
-        sys.stderr.write('error: {0}\n\n'.format(message))
-        self.print_help()
-        sys.exit(2)
+	"""Like *argparse.ArgumentParser*, but prints help when no arguments."""
+	def error(self, message):
+		"""Prints error message, then help."""
+		sys.stderr.write('error: {0}\n\n'.format(message))
+		self.print_help()
+		sys.exit(2)
 
 
 def mapParser():
-    """Returns argument parser for `map_rsv.py`"""
-    parser = argparse.ArgumentParser(description="Plot global distribution "\
-            "of RSV")
-    parser.add_argument(
-            'level', type=str, choices=['subtype', 'genotype'],
-            help="Specify whether the subtype or genotype of RSV sequences "\
-            "should be plotted")
-    parser.add_argument(
-            '--genotype-level', type=str, choices=['collapse', 'all'], 
-            default='collapse', help="Specify whether to plot all genotypes of "\
-            "RSV or collapse them into major clades")
-    parser.add_argument(
-            '--years', default=[1990,2018],
-            help="Specify a range of years to plot")
-    return parser
+	"""Returns argument parser for `map_rsv.py`"""
+	parser = argparse.ArgumentParser(description="Plot global distribution "\
+			"of RSV")
+	parser.add_argument(
+			'level', type=str, choices=['subtype', 'genotype'],
+			help="Specify whether the subtype or genotype of RSV sequences "\
+			"should be plotted")
+	parser.add_argument(
+			'--genotype-level', type=str, choices=['collapse', 'all'], 
+			default='collapse', help="Specify whether to plot all genotypes of "\
+			"RSV or collapse them into major clades")
+	parser.add_argument(
+			'--years', default=[1990,2018],
+			help="Specify a range of years to plot")
+	return parser
 
 def seqParser():
-    """Returns argument parser for `seq_download.py`."""
-    parser = ArgumentParserNoArgHelp(
-            description='Downloads RSV G protein sequences & metadata from '\
-            'Genbank. This program is part of {0} (version {1}) written by '\
-            '{2}.'.format(RSView.__name__, RSView.__version__, 
-            RSView.__author__) + 'For documentation, see {0}'.format(
-            RSView.__url__), 
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--email', required=True, type=str, help='User email'\
-            ' for GenBank download.')
-    parser.add_argument('--query', required=True, type=str, help='Search '\
-            'term(s) for downloading sequences from GenBank.')
-    parser.add_argument('--outdir', required=True, type=str, help='Directory'\
-            'for downloaded data.')
-    parser.add_argument('--outprefix', default='RSVG_gb_metadata', type=str, 
-            help='Beginning of file name for output `.csv`. Suffix will '\
-            'specify number of sequences downloaded.')
-    parser.add_argument('--db', default='nuccore', type=str, help='Entrez'\
-            ' database to search.')
-    parser.add_argument('--firstseq', default=0, type=int, help='Index of '\
-            'first sequence to download.')
-    parser.add_argument('--filesize', default=5000, type=int, help='Number of '\
-            'seqs to download into one file. Default of 5000 balances '\
-            'download time and minimizing the number of separate files.')
-    parser.add_argument('--maxseqs', default=20000, type=int, help='Maximum '\
-            'number of sequences to download across all output files.')
-    parser.add_argument('--batchsize', default=100, type=int, help='Number '\
-            'seqs to download in one retrieval. If much larger than 100, '\
-            'download will be quite slow.')
-    parser.add_argument('--filetype', default='gb', type=str, help='File '\
-            'type of sequences downloaded from GenBank.')
-    parser.add_argument('--outmode', default='xml', type=str, help='File '\
-            'type for results of GenBank query.')
+	"""Returns argument parser for `seq_download.py`."""
+	parser = ArgumentParserNoArgHelp(
+			description='Downloads RSV G protein sequences & metadata from '\
+			'Genbank. This program is part of {0} (version {1}) written by '\
+			'{2}.'.format(RSView.__name__, RSView.__version__, 
+			RSView.__author__) + 'For documentation, see {0}'.format(
+			RSView.__url__), 
+			formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser.add_argument('--email', required=True, type=str, help='User email'\
+			' for GenBank download.')
+	parser.add_argument('--query', required=True, type=str, help='Search '\
+			'term(s) for downloading sequences from GenBank.')
+	parser.add_argument('--outdir', required=True, type=str, help='Directory'\
+			'for downloaded data.')
+	parser.add_argument('--outprefix', default='RSVG_gb_metadata', type=str, 
+			help='Beginning of file name for output `.csv`. Suffix will '\
+			'specify number of sequences downloaded.')
+	parser.add_argument('--db', default='nuccore', type=str, help='Entrez'\
+			' database to search.')
+	parser.add_argument('--firstseq', default=0, type=int, help='Index of '\
+			'first sequence to download.')
+	parser.add_argument('--filesize', default=5000, type=int, help='Number of '\
+			'seqs to download into one file. Default of 5000 balances '\
+			'download time and minimizing the number of separate files.')
+	parser.add_argument('--maxseqs', default=20000, type=int, help='Maximum '\
+			'number of sequences to download across all output files.')
+	parser.add_argument('--batchsize', default=100, type=int, help='Number '\
+			'seqs to download in one retrieval. If much larger than 100, '\
+			'download will be quite slow.')
+	parser.add_argument('--filetype', default='gb', type=str, help='File '\
+			'type of sequences downloaded from GenBank.')
+	parser.add_argument('--outmode', default='xml', type=str, help='File '\
+			'type for results of GenBank query.')
 
-    return parser
+	return parser
 
 def genotypeParser():
-    """Returns argparser for genotype.py"""
-    parser = ArgumentParserNoArgHelp(
-            description='Given RSV G protein sequences & metadata downloaded '\
-            'from Genbank, fill in missing genotype data. This program is part '\
-            ' of {0} (version {1}) written by {2}.'.format(RSView.__name__, 
-            RSView.__version__, RSView.__author__) + 'For documentation, see '
-            '{0}'.format(RSView.__url__), 
-            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--inprefix', required=True, type=str, help="Prefix "\
-            "pointing to downloaded sequences and metadata files. --inprefix"\
-            " name should be truncated where differences begin between files"\
-            " that will be combined into one dataframe. Example: "\
-            "'./data/RSVG_gb_metadata'. ")
-    parser.add_argument('--seqsdir', required=True, type=str, help="Directory"\
-            " for outputting generated fasta and alignment files.")
-    parser.add_argument('--outfile', required=True, type=str, help="Output "\
-            "file for cleaned, genotyped `.csv`.")
-    parser.add_argument('--threshold', type=int, default=150, 
-            help="Threshold for how many sites must match in order to call a"\
-            " genotype.")
-    return parser
+	"""Returns argparser for genotype.py"""
+	parser = ArgumentParserNoArgHelp(
+			description='Given RSV G protein sequences & metadata downloaded '\
+			'from Genbank, fill in missing genotype data. This program is part '\
+			' of {0} (version {1}) written by {2}.'.format(RSView.__name__, 
+			RSView.__version__, RSView.__author__) + 'For documentation, see '
+			'{0}'.format(RSView.__url__), 
+			formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+	parser.add_argument('--inprefix', required=True, type=str, help="Prefix "\
+			"pointing to downloaded sequences and metadata files. --inprefix"\
+			" name should be truncated where differences begin between files"\
+			" that will be combined into one dataframe. Example: "\
+			"'./data/RSVG_gb_metadata'. ")
+	parser.add_argument('--seqsdir', required=True, type=str, help="Directory"\
+			" for outputting generated fasta and alignment files.")
+	parser.add_argument('--outfile', required=True, type=str, help="Output "\
+			"file for cleaned, genotyped `.csv`.")
+	parser.add_argument('--threshold', type=int, default=150, 
+			help="Threshold for how many sites must match in order to call a"\
+			" genotype.")
+	return parser
 
 def plotParser(allowedData=""):
-    parser = argparse.ArgumentParser(description="Plot data on child death "\
-            "rates from acute respiratory infection", 
-            formatter_class=RawTextHelpFormatter)
-    parser.add_argument(
-            'level', type=str, choices=['all', 'country'], default='all',
-            help="Specify whether to plot data for all countries or for a "\
-            "specific country")
-    parser.add_argument(
-            'data_type', type=str, choices=[' nnd ', ' pnd ', ' neo9 ',
-            ' post9 ', ' ufive9 ', ' rneo9 ', ' rpost9 ', ' rufive9 ', 
-            'fneo9', 'fpost9', 'fufive9'], help="Specify which category of "\
-            "data to plot:\n" + allowedData)
-    parser.add_argument(
-        '--country', type=str, default='Global',
-        help="Specify the country for which to plot data",
-        #required='level' in sys.argv and sys.args.level == "country"
-        )
-    parser.add_argument(
-        '--highlight_country', type=str, default=None,
-        help="Specify the country for to highlight")
-    return parser
+	parser = argparse.ArgumentParser(description="Plot data on child death "\
+			"rates from acute respiratory infection", 
+			formatter_class=RawTextHelpFormatter)
+	parser.add_argument(
+			'level', type=str, choices=['all', 'country'], default='all',
+			help="Specify whether to plot data for all countries or for a "\
+			"specific country")
+	parser.add_argument(
+			'data_type', type=str, choices=[' nnd ', ' pnd ', ' neo9 ',
+			' post9 ', ' ufive9 ', ' rneo9 ', ' rpost9 ', ' rufive9 ', 
+			'fneo9', 'fpost9', 'fufive9'], help="Specify which category of "\
+			"data to plot:\n" + allowedData)
+	parser.add_argument(
+		'--country', type=str, default='Global',
+		help="Specify the country for which to plot data",
+		#required='level' in sys.argv and sys.args.level == "country"
+		)
+	parser.add_argument(
+		'--highlight_country', type=str, default=None,
+		help="Specify the country for to highlight")
+	return parser
+
+
+
+def correlationParser(allowedData=""):
+	parser = argparse.ArgumentParser(description="Plot data on child death "\
+		"rates from acute respiratory infection", 
+		formatter_class=RawTextHelpFormatter)
+	parser.add_argument(
+		'level', type=str, choices=['all', 'year'], default='all',
+		help="Specify whether to plot data for all countries or for a "\
+		"specific country")
+	parser.add_argument(
+		'data_type', type=str, choices=[' nnd ', ' pnd ', ' neo9 ',
+		' post9 ', ' ufive9 ', ' rneo9 ', ' rpost9 ', ' rufive9 ', 
+		'fneo9', 'fpost9', 'fufive9'], help="Specify which category of "\
+		"data to plot:\n" + allowedData)
+	return parser
+
+
 
--- RSView/plot_correlation.py
+++ RSView/plot_correlation.py
@@ -5,66 +5,99 @@ import plotly.plotly as py
 import plotly.graph_objs as go
 import country_converter as coco
 import plot_rsv
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+import seaborn as sns
+
+
+from parsearguments import correlationParser
 
 def country_to_iso3(country):
-    if country == 'Global':
-        return 'Global'
-    else:
-        iso3 =  coco.convert(names=country, to='ISO3')
-        return iso3
+	if country == 'Global':
+		return 'Global'
+	else:
+		iso3 =  coco.convert(names=country, to='ISO3')
+		return iso3
 
 def count_subtypes(rsv_df):
-    """
-    Restructure the DataFrame so that each row indicates the total number of RSV sequences
-    found in each country, each year, for each subtype
-    """
-    
-    #count number of rows(seqs) from each country that are each subtype
-    df_group = pd.DataFrame({'count' : rsv_df.groupby(['country', 'subtype']).size()}).reset_index()
+	"""
+	Restructure the DataFrame so that each row indicates the total number of RSV sequences
+	found in each country, each year, for each subtype
+	"""
+	
+	#count number of rows(seqs) from each country that are each subtype
+	df_group = pd.DataFrame({'count' : rsv_df.groupby(['country', 'subtype']).size()}).reset_index()
 
-    return df_group
+	return df_group
 
 
 def count_subtypes_year(rsv_df):
-    """
-    Restructure the DataFrame so that each row indicates the total number of RSV sequences
-    found in each country, each year, for each subtype
-    """
-    
-    #count number of rows(seqs) from each country that are each subtype
-    df_group = pd.DataFrame({'count' : rsv_df.groupby(['country', 'subtype',
-                                                           'year']).size()}).reset_index()
+	"""
+	Restructure the DataFrame so that each row indicates the total number of RSV sequences
+	found in each country, each year, for each subtype
+	"""
+	
+	#count number of rows(seqs) from each country that are each subtype
+	df_group = pd.DataFrame({'count' : rsv_df.groupby(['country', 'subtype',
+														   'year']).size()}).reset_index()
 
-    return df_group
+	return df_group
 
 def get_ratio_all(df):
-    df_ratio = pd.DataFrame(columns=['country', 'ratio', 'count_A', 'count_B', 'count_total'])
-    countries = df['country'].unique()
-    for i in countries:
-        country_df = df[df['country']==i]
-        if country_df['subtype'].count() == 1:
-            lone_subtype = country_df['subtype'].item()
-            if lone_subtype == 'A':
-                percent_A = 1
-                count_A = country_df['count'].item()
-                count_B = 0
-                count_total = count_A + count_B
+	df_ratio = pd.DataFrame(columns=['country', 'ratio', 'count_A', 'count_B', 'count_total'])
+	countries = df['country'].unique()
+	for i in countries:
+		country_df = df[df['country']==i]
+		if country_df['subtype'].count() == 1:
+			lone_subtype = country_df['subtype'].item()
+			if lone_subtype == 'A':
+				percent_A = 1
+				count_A = country_df['count'].item()
+				count_B = 0
+				count_total = count_A + count_B
 
-            else:
-                percent_A = 0
-                count_A = 0
-                count_B = country_df['count'].item()
-                count_total = count_A + count_B
-        else:
-            count_A = float(country_df[country_df['subtype'] == 'A']['count'])
-            count_B = float(country_df[country_df['subtype'] == 'B']['count'])
-            percent_A = count_A/(count_A + count_B)
-            count_total = count_A + count_B
-        df_ratio = df_ratio.append(pd.Series([i, percent_A, count_A, count_B, count_total], index=df_ratio.columns, name=i))
-    return df_ratio
+			else:
+				percent_A = 0
+				count_A = 0
+				count_B = country_df['count'].item()
+				count_total = count_A + count_B
+		else:
+			count_A = float(country_df[country_df['subtype'] == 'A']['count'])
+			count_B = float(country_df[country_df['subtype'] == 'B']['count'])
+			percent_A = count_A/(count_A + count_B)
+			count_total = count_A + count_B
+		df_ratio = df_ratio.append(pd.Series([i, percent_A, count_A, count_B, count_total], index=df_ratio.columns, name=i))
+	return df_ratio
+
+def get_ratio_year(df):
+	df_ratio = pd.DataFrame(columns=['country', 'year', 'ratio', 'count_A', 'count_B', 'count_total'])
+	countries = df['country'].unique()
+	for i in countries:
+		country_df = df[df['country']==i]
+		years = country_df['year'].unique()
+		for j in years:
+			years_df = country_df[country_df['year']==j]
+			if years_df['subtype'].count() == 1:
+				lone_subtype = years_df['subtype'].item()
+				if lone_subtype == 'A':
+					percent_A = 1
+					count_A = years_df['count'].item()
+					count_B = 0
+					count_total = count_A + count_B
+				else:
+					percent_A = 0
+					count_A = 0
+					count_B = years_df['count'].item()
+					count_total = count_A + count_B
+			else:
+				count_A = float(years_df[years_df['subtype'] == 'A']['count'])
+				count_B = float(years_df[years_df['subtype'] == 'B']['count'])
+				percent_A = count_A/(count_A + count_B)
+				count_total = count_A + count_B
+			df_ratio = df_ratio.append(pd.Series([i, j, percent_A, count_A, count_B, count_total], index=df_ratio.columns, name=i))
+	return df_ratio
 
 
-def merge_ratio_health(df_ratio)
+def merge_ratio_health(df_ratio):
 	
 	df_health_summary = pd.read_csv('./data/health_data_summary.csv')
 	df_ratio["iso3"] = [country_to_iso3(x) for x in df_ratio.country.values]
@@ -75,50 +108,115 @@ def merge_ratio_health(df_ratio)
 
 
 
+
+
 def plot_ratio(df_merge, data_type):
 
-	trace = go.Scatter(
-	    x = df_merge['ratio'],
-	    y = df_merge[data_type],
-	    mode = 'markers',
-	    text = df_merge['country_short'] # The hover text goes here... 
+	trace1 = go.Scatter(
+		x = df_merge['ratio'],
+		y = df_merge[data_type],
+		mode = 'markers',
+		marker= dict(size= 14,
+					line= dict(width=1),
+					color='rgba(204,204,204,1)'
+				   ),
+		text = df_merge['country_short'] # The hover text goes here... 
 	)
 
 	layout= go.Layout(
-	    title= 'RSV Subtype Prevalence compared to ' + plot_rsv.DATA_DICT[data_type],
-	    hovermode= 'closest',
-	    xaxis= dict(
-	        title= 'Ratio of Subtype A over Subtype B Sequences Recorded',
-	        # ticklen= 5,
-	        # zeroline= False,
-	        # gridwidth= 2,
-	    ),
-	    yaxis=dict(
-	        title= plot_rsv.DATA_DICT[data_type],
-	        # ticklen= 5,
-	        # gridwidth= 2,
-	    ),
-	    showlegend= False
+		title= 'RSV Subtype Prevalence compared to ' + plot_rsv.DATA_DICT[data_type],
+		hovermode= 'closest',
+		xaxis= dict(
+			title= 'Ratio of Subtype A over Subtype B Sequences Recorded',
+			# ticklen= 5,
+			# zeroline= False,
+			# gridwidth= 2,
+		),
+		yaxis=dict(
+			title = plot_rsv.DATA_DICT[data_type],
+			# ticklen= 5,
+			# gridwidth= 2,
+		),
+		showlegend = False
 	)
+
+	data = [trace1]
+
+	fig= go.Figure(data=data, layout=layout)
+	plot(fig)
+
+def plot_ratio_year(df_merge, data_type):
+
+	df_merge['hover'] = df_merge['country_short'] + ', ' + df_merge['year'].astype(int).map(str)
+
+	trace1 = go.Scatter(
+		x = df_merge['ratio'],
+		y = df_merge[data_type],
+		mode = 'markers',
+		marker= dict(size= 14,
+					line= dict(width=1),
+					color=df_merge['year'],
+					#colorscale='Cividis',
+					showscale=True,
+					cmin=1990,
+					cmax=2018
+				   ),
+		text = df_merge['hover'] # The hover text goes here... 
+	)
+
+	layout= go.Layout(
+		title= 'RSV Subtype Prevalence compared to ' + plot_rsv.DATA_DICT[data_type],
+		hovermode= 'closest',
+		xaxis= dict(
+			title= 'Ratio of Subtype A over Subtype B Sequences Recorded',
+			# ticklen= 5,
+			# zeroline= False,
+			# gridwidth= 2,
+		),
+		yaxis=dict(
+			title = plot_rsv.DATA_DICT[data_type],
+			# ticklen= 5,
+			# gridwidth= 2,
+		),
+		showlegend = False
+	)
+
+	data = [trace1]
 
 	fig= go.Figure(data=data, layout=layout)
 	plot(fig)
 
 
 
-
-
-def main(data_type):
-    """
-    Run organize_data, count_types, map_rsv
-    """
+def main(level, data_type):
+	"""
+	Run organize_data, count_types, map_rsv
+	"""
 	rsv_df = map_rsv.organize_data(map_rsv.DATAFILES, map_rsv.GENOTYPE_DICT)
 	rsv_df = rsv_df.dropna(subset = ['subtype']).copy()
 
-	rsv_df_count = count_subtypes(rsv_df)
+	rsv_df_year = rsv_df.dropna(subset = ['year']).copy()
+	rsv_df_year['year'] = rsv_df_year['year'].astype(int)
 
-	df_ratio_all = get_ratio_all(rsv_df_count)
+	if level == 'all':
+		rsv_df_count = count_subtypes(rsv_df)
+		df_ratio_all = get_ratio_all(rsv_df_count)
+		df_merged = merge_ratio_health(df_ratio_all)
+		plot_ratio(df_merged, data_type)
 
+	else:
+		rsv_df_count = count_subtypes_year(rsv_df)
+		df_ratio_year = get_ratio_year(rsv_df_count)
+		df_merged = merge_ratio_health(df_ratio_year)
+		plot_ratio_year(df_merged, data_type)
+
+	
+
+if __name__ == "__main__":
+
+	args = correlationParser(plot_rsv.dictToHelp(plot_rsv.DATA_DICT)).parse_args()
+
+	main(args.level, args.data_type)
 
 
 
